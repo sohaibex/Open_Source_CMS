0d2977c147f7880c3a02dca2ba5652f0ca496a40
SOHAIB EL MEDIOUNI <sohaib.elmediouni23@gmail.com>
2021-03-21 20:39:04 +0100

    auth

---
+++ views/auth/login.php
@@ -0,0 +1,85 @@
+<?php include '../index.php' ?>
+<div class="account-pages my-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-10">
+                <div class="card">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div class="col-md-12 p-5">
+                                <div class="mx-auto mb-5">
+                                    <a href="/">
+                                        <h3 class="d-inline align-middle ml-1 text-logo">CMS</h3>
+                                    </a>
+                                </div>
+
+                                <h6 class="h5 mb-0 mt-4">Welcome back!</h6>
+                                <p class="text-muted mt-1 mb-4">Enter your email address and password to
+                                    access CMS.</p>
+
+                                <form action="" method="post" class="authentication-form">
+
+
+                                    <div class="form-group">
+                                        <label class="form-control-label">Email Address</label>
+                                        <div class="input-group input-group-merge">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fas fa-envelope"></i>
+                                                </span>
+                                            </div>
+                                            <input type="email" class="form-control " id="
+                                                email" placeholder="hello@coderthemes.com" name="email" value="" />
+
+
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group mt-4">
+                                        <label class="form-control-label">Password</label>
+                                        <a href="" class="float-right text-muted text-unline-dashed ml-1">Forgot your
+                                            password?</a>
+                                        <div class="input-group input-group-merge">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text">
+                                                    <i class="fas fa-lock"></i>
+                                                </span>
+                                            </div>
+                                            <input type="password" class="form-control" id="password" placeholder="Enter your password" name="password" />
+
+
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group mb-4">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="checkbox-signin" />
+                                            <label class="custom-control-label" for="checkbox-signin">Remember me</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group mb-0 text-center">
+                                        <button class="btn btn-primary btn-block" type="submit"> Log In
+                                        </button>
+                                    </div>
+                                </form>
+
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="row mt-3">
+                    <div class="col-12 text-center">
+                        <p class="text-muted">Don't have an account? <a href="/Open_Source_CMS/views" class="text-primary font-weight-bold ml-1">Sign Up</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
